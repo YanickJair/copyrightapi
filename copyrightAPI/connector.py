89d7f7b37fa2404f1a1bc80b7d12c915cace2689
Yanick Andrade <yanick@localhost.localdomain>
2018-08-08 02:40:35 -0100

    preparing repo

---
+++ copyrightAPI/connector.py
@@ -0,0 +1,17 @@
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+DATABASES = {
+   'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'copyright',
+        'USER': 'root',
+        'PASSWORD': 'YanickTeixeira#1310',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
